49cc1f2b8495a702b7610f9d3abf6073be8f0a3b
Ernesto Acevedo <eacevedo@debian.cl>
2014-05-05 00:33:24 -0400

    Se implementa Codeigniter más BD de testing en SQLite

--- application/views/view_index.php
+++ application/views/view_index.php
@@ -3,12 +3,12 @@
 <head>
 	<meta charset="UTF-8" />
 	<title>Sistema de Reserva de Salas</title>
-	<link rel="stylesheet" href="css/bootstrap.min.css" />
-	<link rel="stylesheet" href="css/font-awesome.min.css" />
-	<link rel="stylesheet" href="css/custom.css" />
-	<link rel="stylesheet" href="css/aui-scheduler-view-week.css" />
-	<link rel="stylesheet" href="css/calendar.css" />
-	<link rel="stylesheet" href="css/custom_2.css" />
+	<link rel="stylesheet" href="<?php echo base_url(); ?>css/bootstrap.min.css" />
+	<link rel="stylesheet" href="<?php echo base_url(); ?>css/font-awesome.min.css" />
+	<link rel="stylesheet" href="<?php echo base_url(); ?>css/custom.css" />
+	<link rel="stylesheet" href="<?php echo base_url(); ?>css/aui-scheduler-view-week.css" />
+	<link rel="stylesheet" href="<?php echo base_url(); ?>css/calendar.css" />
+	<link rel="stylesheet" href="<?php echo base_url(); ?>css/custom_2.css" />
 	<link href="http://cdn.alloyui.com/2.5.0/aui-css/css/bootstrap.min.css" rel="stylesheet"></link>
 </head>
 <body>
@@ -44,11 +44,11 @@
 			
 		</footer>
 	</section>
-	<script src="js/jquery.js"></script>
-	<script src="js/bootstrap.min.js"></script>
-	<script src="js/aui.js"></script>
-	<script src="js/jquery.calendario.js"></script>
-	<script src="js/modernizr.custom.63321.js"></script>
-	<script src="js/custom.js"></script>
+	<script src="<?php echo base_url(); ?>js/jquery.js"></script>
+	<script src="<?php echo base_url(); ?>js/bootstrap.min.js"></script>
+	<script src="<?php echo base_url(); ?>js/aui.js"></script>
+	<script src="<?php echo base_url(); ?>js/jquery.calendario.js"></script>
+	<script src="<?php echo base_url(); ?>js/modernizr.custom.63321.js"></script>
+	<script src="<?php echo base_url(); ?>js/custom.js"></script>
 </body>
 </html>
