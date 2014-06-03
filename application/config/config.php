2cd419ee0b325b6076ef1506545bc7a32847b5a1
Diego Vergara <reiby-viper@hotmail.com>
2014-06-03 00:33:06 -0400

    Url Base y seguridad por encriptación

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= '';
+$config['base_url']	= 'reserva-salas';
 
 /*
 |--------------------------------------------------------------------------
@@ -224,7 +224,7 @@ $config['cache_path'] = '';
 | MUST set an encryption key.  See the user guide for info.
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = '23456789087654345678987654345678765434567897654567898765';
 
 /*
 |--------------------------------------------------------------------------
